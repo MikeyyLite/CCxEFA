e7b37829fa30b63fe3245fe5106bfbd83effeb96
MikeyyLite <satvikbeli@gmail.com>
2023-02-06 21:25:31 +0530

    modified:   ccxefa/lib/main.dart 	modified:   ccxefa/lib/news_page.dart 	modified:   ccxefa/pubspec.lock 	modified:   ccxefa/pubspec.yaml

--- ccxefa/lib/main.dart
+++ ccxefa/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_sliding_tutorial/flutter_sliding_tutorial.dart';
 import 'package:flutter/gestures.dart';
 import 'sliding_pages/slider.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'login_page.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'news_page.dart';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -54,41 +57,43 @@ class _ExamplePageState extends State<ExamplePage> {
     height = size.height;
     width = size.width;
 
-    return Scaffold(
-      body: Center(
-        child: Stack(children: <Widget>[
-          /// [StatefulWidget] with [PageView] and [AnimatedBackgroundColor].
-          SlidingTutorial(
-            controller: _pageCtrl,
-            pageCount: pageCount,
-            notifier: notifier,
-          ),
+    final pagebody = Center(
+      child: Stack(children: <Widget>[
+        /// [StatefulWidget] with [PageView] and [AnimatedBackgroundColor].
+        SlidingTutorial(
+          controller: _pageCtrl,
+          pageCount: pageCount,
+          notifier: notifier,
+        ),
 
-          Align(
-              alignment: const Alignment(0, 0.5),
-              child: SmoothPageIndicator(
-                  controller: _pageCtrl,
-                  // PageController
-                  count: 4,
-                  effect: ExpandingDotsEffect(
-                      dotColor: Color(0xFF30CEDF),
-                      activeDotColor: Color(0xFF30CEDF),
-                      dotHeight: 10,
-                      dotWidth: 10), // your preferred effect
-                  onDotClicked: (index) {})),
-          Align(
-              alignment: const Alignment(0, 0.9),
-              child: GestureDetector(
-                onTap: () {
-                  _pageCtrl.nextPage(
-                    duration: const Duration(milliseconds: 200),
-                    curve: Curves.linear,
-                  );
-                },
-                child: SvgPicture.asset('images/next_button.svg'),
-              ))
-        ]),
-      ),
+        Align(
+            alignment: const Alignment(0, 0.5),
+            child: SmoothPageIndicator(
+                controller: _pageCtrl,
+                // PageController
+                count: 4,
+                effect: ExpandingDotsEffect(
+                    dotColor: Color(0xFF30CEDF),
+                    activeDotColor: Color(0xFF30CEDF),
+                    dotHeight: 10,
+                    dotWidth: 10), // your preferred effect
+                onDotClicked: (index) {})),
+        Align(
+            alignment: const Alignment(0, 0.9),
+            child: GestureDetector(
+              onTap: () {
+                _pageCtrl.nextPage(
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.linear,
+                );
+              },
+              child: SvgPicture.asset('images/next_button.svg'),
+            ))
+      ]),
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(child: pagebody)
+        : Scaffold(body: pagebody);
   }
 }
--- ccxefa/lib/news_page.dart
+++ ccxefa/lib/news_page.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:io';
 
 //incomplete
 
@@ -29,115 +32,133 @@ class ListViewBuilder extends StatelessWidget {
     size = MediaQuery.of(context).size;
     height = size.height;
     width = size.width;
-
-    return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-            leading: IconButton(
-              color: Colors.white,
-
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.arrow_back_ios),
-              //replace with our own icon data.
-            ),
-            centerTitle: true,
-            backgroundColor: Colors.black,
-            title: Text("News",
-                style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFFFFFFFF),
-                      fontWeight: FontWeight.w600),
-                ))),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.only(
-                left: 10,
-                top: 15,
-                right: 10,
+    final pagebody = ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+            top: 15,
+            right: 10,
+          ),
+          child: Container(
+            height: height * 1 / 8,
+            width: width * 34 / 36,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
               ),
-              child: Container(
-                height: height * 1 / 8,
-                width: width * 34 / 36,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                  ),
-                  child: Row(children: <Widget>[
-                    Container(
-                      height: height * 1 / 8,
-                      width: width * 10 / 36,
-                      // child: Column(children: [
-                      //   SvgPicture.asset(
-                      //     'images/Rectangle2.svg',
-                      //   ),
-                      // ]),
-                    ),
-                    Container(
-                        // color: Colors.red,
-                        width: width * 223 / 360,
-                        height: height * 1 / 8,
-                        child: Container(
-                          // color: Colors.blue,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                // color: Colors.black,
-                                height: height * 37 / 800,
-                                width: width * 223 / 360,
-                                // color: Colors.black,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    TextButton(
-                                      onPressed: () {},
-                                      child: Align(
-                                          alignment: Alignment.topLeft,
-                                          child: Container(
-                                              // height: height * 1 / 80,
-                                              // color: Colors.amber,
-                                              child: Text('PRODUCTIVITY',
-                                                  style: TextStyle(
-                                                      fontSize:
-                                                          height * 11 / 800,
-                                                      color:
-                                                          Color(0xFF6E798C))))),
-                                    ),
-                                    // SizedBox(width: 27)
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                // color: Colors.amber,
-                                height: height * 63 / 800 - 8,
-                                width: width * 223 / 360,
-                                child: TextButton(
-                                    onPressed: () {},
-                                    child: Align(
-                                        alignment: Alignment.topCenter,
-                                        child: Text(
-                                          '7 Skills of Highly Effective Programmers',
-                                          style: TextStyle(
-                                              fontSize: height * 14 / 800,
-                                              fontWeight: FontWeight.w600,
-                                              color: Color(0xFF081F32)),
-                                        ))),
-                              )
-                            ],
-                          ),
-                        ))
-                  ]),
+              child: Row(children: <Widget>[
+                Container(
+                  height: height * 1 / 8,
+                  width: width * 10 / 36,
+                  // child: Column(children: [
+                  //   SvgPicture.asset(
+                  //     'images/Rectangle2.svg',
+                  //   ),
+                  // ]),
                 ),
-              ),
-            );
-          },
-        ));
+                Container(
+                    // color: Colors.red,
+                    width: width * 223 / 360,
+                    height: height * 1 / 8,
+                    child: Container(
+                      // color: Colors.blue,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            // color: Colors.black,
+                            height: height * 37 / 800,
+                            width: width * 223 / 360,
+                            // color: Colors.black,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Container(
+                                          // height: height * 1 / 80,
+                                          // color: Colors.amber,
+                                          child: Text('PRODUCTIVITY',
+                                              style: TextStyle(
+                                                  fontSize: height * 11 / 800,
+                                                  color: Color(0xFF6E798C))))),
+                                ),
+                                // SizedBox(width: 27)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            // color: Colors.amber,
+                            height: height * 63 / 800 - 8,
+                            width: width * 223 / 360,
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Text(
+                                      '7 Skills of Highly Effective Programmers',
+                                      style: TextStyle(
+                                          fontSize: height * 14 / 800,
+                                          fontWeight: FontWeight.w600,
+                                          color: Color(0xFF081F32)),
+                                    ))),
+                          )
+                        ],
+                      ),
+                    ))
+              ]),
+            ),
+          ),
+        );
+      },
+    );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pagebody,
+            navigationBar: CupertinoNavigationBar(
+                leading: IconButton(
+                    color: Colors.white,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back_ios)),
+                backgroundColor: Colors.black,
+                middle: Text("News",
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFFFFFFFF),
+                          fontWeight: FontWeight.w600),
+                    ))),
+          )
+        : Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+                leading: IconButton(
+                  color: Colors.white,
+
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back_ios),
+                  //replace with our own icon data.
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.black,
+                title: Text("News",
+                    style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFFFFFFFF),
+                          fontWeight: FontWeight.w600),
+                    ))),
+            body: pagebody);
   }
 }
